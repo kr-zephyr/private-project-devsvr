eac75bf852b541605f2f57c250a369cd48454f60
SeungChul Lee <scin21c@gmail.com>
2015-09-14 13:29:00 +0900

    System.out 제거

--- src/main/java/com/sz21c/flightlogger/settings/controller/ManageAircraftController.java
+++ src/main/java/com/sz21c/flightlogger/settings/controller/ManageAircraftController.java
@@ -6,15 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Created by zephyr on 15. 9. 8..
- */
 @Controller
 public class ManageAircraftController extends FlightLoggerBaseController {
 
     @RequestMapping(value = "/manage/aircraft", method = {RequestMethod.GET})
     public ModelAndView getList() throws Exception {
-        System.out.println("call /manage/aircraft");
         return new ModelAndView("/flightlogger/manage-aircraft-list");
     }
 }
