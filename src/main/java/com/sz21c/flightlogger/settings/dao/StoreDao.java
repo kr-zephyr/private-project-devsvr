1d6a3576c895c6fde992517e85281bcb94924012
SeungChul Lee <scin21c@gmail.com>
2015-09-15 13:07:25 +0900

    store의 CRUD mapper 구현

---
+++ src/main/java/com/sz21c/flightlogger/settings/dao/StoreDao.java
@@ -0,0 +1,19 @@
+package com.sz21c.flightlogger.settings.dao;
+
+import com.sz21c.flightlogger.settings.model.StoreVO;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository(value="StoreDao")
+public interface StoreDao {
+    List<StoreVO> selectMstStore(StoreVO storeVO) throws Exception;
+
+    StoreVO selectMstStoreById(Integer id) throws Exception;
+
+    StoreVO insertMstStore(StoreVO storeVO) throws Exception;
+
+    StoreVO updateMstStoreById(StoreVO storeVO) throws Exception;
+
+    Integer deleteMstStoreById(Integer id) throws Exception;
+}
