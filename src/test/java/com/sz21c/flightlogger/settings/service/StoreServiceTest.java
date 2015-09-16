5375d14618e6bc489251c7b504c99ca8e58a802e
SeungChul Lee <scin21c@gmail.com>
2015-09-16 13:09:21 +0900

    StoreService Test 추가

---
+++ src/test/java/com/sz21c/flightlogger/settings/service/StoreServiceTest.java
@@ -0,0 +1,63 @@
+package com.sz21c.flightlogger.settings.service;
+
+import com.sz21c.common.config.SpringRootConfig;
+import com.sz21c.flightlogger.settings.model.StoreVO;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {SpringRootConfig.class})
+//TODO TransactionConfiguration deprecate 되었으니 변경해야 함
+@TransactionConfiguration(transactionManager = "dataSourceTransactionManager", defaultRollback = true)
+@Transactional
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class StoreServiceTest {
+
+    private Logger logger = LoggerFactory.getLogger(StoreServiceTest.class);
+
+    private final String STORE_NAME = "test-store";
+    private final String STORE_SITE_URL = "http://www.test-store.com";
+
+    @Autowired
+    StoreService storeService;
+
+    @Test
+    public void test_001_AddStore() throws Exception {
+        StoreVO storeVO = new StoreVO();
+        storeVO.setName(STORE_NAME);
+        storeVO.setSiteUrl(STORE_SITE_URL);
+        storeVO.setRowCreateTime(System.currentTimeMillis());
+        storeVO.setRowModifyTime(System.currentTimeMillis());
+
+        storeService.addStore(storeVO);
+
+        assertNotNull(storeVO.getId());
+        assertNotEquals(storeVO.getId(), new Integer(0));
+
+        logger.info("created id :: " + storeVO.getId());
+    }
+
+    @Test
+    public void test_002_GetStore() throws Exception {
+        List<StoreVO> storeList = storeService.getStoreList();
+
+        assertNotNull(storeList);
+
+        logger.info("store list size is " + storeList.size());
+        logger.info("last created store is " + storeList.get(0).getName());
+        logger.info("id of last created store is " + storeList.get(0).getId());
+    }
+}
