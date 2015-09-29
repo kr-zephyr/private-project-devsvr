78b51944264abbf8506b1a5e09a9a9cff6a3fb93
SeungChul Lee <scin21c@gmail.com>
2015-09-30 00:31:23 +0900

    Front에 노출시킬 view model 추가

--- src/main/java/com/sz21c/flightlogger/settings/model/StoreViewModel.java
+++ src/main/java/com/sz21c/flightlogger/settings/model/StoreViewModel.java
@@ -4,12 +4,15 @@ import com.sz21c.common.util.TimeUtil;
 
 public class StoreViewModel {
     private StoreVO store;
-    private String dateString;
+    private String addedDateString;
+    private String modifiedDateString;
+    private Integer registeredAppCount;
 
     public StoreViewModel(StoreVO storeVO) {
         if(storeVO != null) {
             this.store = storeVO;
-            this.dateString = TimeUtil.getDateStringFromMilliTime(this.store.getRowCreateTime());
+            this.addedDateString = TimeUtil.getDateStringFromMilliTime(this.store.getRowCreateTime());
+            this.modifiedDateString = TimeUtil.getDateStringFromMilliTime(this.store.getRowModifyTime());
         }
     }
 
@@ -17,7 +20,19 @@ public class StoreViewModel {
         return store;
     }
 
-    public String getDateString() {
-        return dateString;
+    public String getAddedDateString() {
+        return addedDateString;
+    }
+
+    public String getModifiedDateString() {
+        return modifiedDateString;
+    }
+
+    public Integer getRegisteredAppCount() {
+        return registeredAppCount;
+    }
+
+    public void setRegisteredAppCount(Integer registeredAppCount) {
+        this.registeredAppCount = registeredAppCount;
     }
 }
