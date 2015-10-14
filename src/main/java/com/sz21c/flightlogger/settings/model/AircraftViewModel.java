7a7ee30715feb168f4f356daa3a77835ee4d4228
SeungChul Lee <scin21c@gmail.com>
2015-10-15 02:21:44 +0900

    항공기 view model에 getter, setter 추가

--- src/main/java/com/sz21c/flightlogger/settings/model/AircraftViewModel.java
+++ src/main/java/com/sz21c/flightlogger/settings/model/AircraftViewModel.java
@@ -14,4 +14,108 @@ public class AircraftViewModel {
     private Integer maxLandingDistance; //착륙 거리
     private Integer maxRange;   //순항 거리
     private Integer maxSpeed;   //순항 속도(knot)
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+    public String getProductUrl() {
+        return productUrl;
+    }
+
+    public void setProductUrl(String productUrl) {
+        this.productUrl = productUrl;
+    }
+
+    public String getCommercialFlag() {
+        return commercialFlag;
+    }
+
+    public void setCommercialFlag(String commercialFlag) {
+        this.commercialFlag = commercialFlag;
+    }
+
+    public String getPurchaseCurrency() {
+        return purchaseCurrency;
+    }
+
+    public void setPurchaseCurrency(String purchaseCurrency) {
+        this.purchaseCurrency = purchaseCurrency;
+    }
+
+    public Integer getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(Integer purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public String getPurchaseDateTime() {
+        return purchaseDateTime;
+    }
+
+    public void setPurchaseDateTime(String purchaseDateTime) {
+        this.purchaseDateTime = purchaseDateTime;
+    }
+
+    public String getSoftwareType() {
+        return softwareType;
+    }
+
+    public void setSoftwareType(String softwareType) {
+        this.softwareType = softwareType;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getMaxTakeoffDistance() {
+        return maxTakeoffDistance;
+    }
+
+    public void setMaxTakeoffDistance(Integer maxTakeoffDistance) {
+        this.maxTakeoffDistance = maxTakeoffDistance;
+    }
+
+    public Integer getMaxLandingDistance() {
+        return maxLandingDistance;
+    }
+
+    public void setMaxLandingDistance(Integer maxLandingDistance) {
+        this.maxLandingDistance = maxLandingDistance;
+    }
+
+    public Integer getMaxRange() {
+        return maxRange;
+    }
+
+    public void setMaxRange(Integer maxRange) {
+        this.maxRange = maxRange;
+    }
+
+    public Integer getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(Integer maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
 }
