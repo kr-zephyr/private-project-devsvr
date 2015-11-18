63da5eb66e8bf2e7912800cadaf2db99658b23a0
SeungChul Lee <scin21c@gmail.com>
2015-11-18 13:06:27 +0900

    lombok annotation 추가

--- src/main/java/com/sz21c/flightlogger/settings/model/StoreVO.java
+++ src/main/java/com/sz21c/flightlogger/settings/model/StoreVO.java
@@ -1,63 +1,16 @@
 package com.sz21c.flightlogger.settings.model;
 
-/**
- * Created by zephyr on 15. 9. 8..
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StoreVO {
     private Integer id;
     private String  name;
     private String  siteUrl;    //TODO url pattern validate해야 함
     private Long    rowCreateTime;
     private Long    rowModifyTime;
-
-    @Override
-    public String toString() {
-        return "StoreVO{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", siteUrl='" + siteUrl + '\'' +
-                ", rowCreateTime=" + rowCreateTime +
-                ", rowModifyTime=" + rowModifyTime +
-                '}';
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getRowCreateTime() {
-        return rowCreateTime;
-    }
-
-    public void setRowCreateTime(Long rowCreateTime) {
-        this.rowCreateTime = rowCreateTime;
-    }
-
-    public Long getRowModifyTime() {
-        return rowModifyTime;
-    }
-
-    public void setRowModifyTime(Long rowModifyTime) {
-        this.rowModifyTime = rowModifyTime;
-    }
-
-    public String getSiteUrl() {
-        return siteUrl;
-    }
-
-    public void setSiteUrl(String siteUrl) {
-        this.siteUrl = siteUrl;
-    }
 }
