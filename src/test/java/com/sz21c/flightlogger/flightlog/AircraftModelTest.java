937f1e6389031c0e1c928c0140ce00b68e3787be
SeungChul Lee <scin21c@gmail.com>
2016-01-05 01:18:50 +0900

    PMDG B777-300의 속성값 변경 -product url -market url

--- src/test/java/com/sz21c/flightlogger/flightlog/AircraftModelTest.java
+++ src/test/java/com/sz21c/flightlogger/flightlog/AircraftModelTest.java
@@ -4,17 +4,19 @@ import com.sz21c.flightlogger.flightlog.model.staticmodel.StaticAircraftModel;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 @Slf4j
 public class AircraftModelTest {
 
     @Test
     public void testStaticModel() throws Exception {
-        log.debug("772 :: " + StaticAircraftModel.PMDG_772.getName());
-        log.debug("772 :: " + StaticAircraftModel.PMDG_772.getDeveloperName());
-        log.debug("772 :: " + StaticAircraftModel.PMDG_772.getProductUrl());
+        assertEquals("B777-200", StaticAircraftModel.PMDG_772.getName());
+        assertEquals("PMDG", StaticAircraftModel.PMDG_772.getDeveloperName());
+        assertEquals("http://www.precisionmanuals.com/pages/product/FSX/777LRF.html", StaticAircraftModel.PMDG_772.getProductUrl());
 
-        log.debug("773 :: " + StaticAircraftModel.PMDG_773.getName());
-        log.debug("773 :: " + StaticAircraftModel.PMDG_773.getDeveloperName());
-        log.debug("773 :: " + StaticAircraftModel.PMDG_773.getProductUrl());
+        assertEquals("B777-300", StaticAircraftModel.PMDG_773.getName());
+        assertEquals("PMDG", StaticAircraftModel.PMDG_773.getDeveloperName());
+        assertEquals("http://www.precisionmanuals.com/pages/product/FSX/773ER.html", StaticAircraftModel.PMDG_773.getProductUrl());
     }
 }
