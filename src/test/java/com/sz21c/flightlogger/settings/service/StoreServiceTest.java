40bb876f090e2b78b41469ced6c8677777647453
SeungChul Lee <scin21c@gmail.com>
2016-01-05 01:31:50 +0900

    사용하지 않는 import 제거

--- src/test/java/com/sz21c/flightlogger/settings/service/StoreServiceTest.java
+++ src/test/java/com/sz21c/flightlogger/settings/service/StoreServiceTest.java
@@ -5,8 +5,6 @@ import com.sz21c.flightlogger.settings.model.StoreVO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
