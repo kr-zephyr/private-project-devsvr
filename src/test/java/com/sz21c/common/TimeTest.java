0fafeb141d33376e1bf76fbe30f76092d57b0831
SeungChul Lee <scin21c@gmail.com>
2016-01-05 01:30:29 +0900

    logger를 Slf4j Annotation 기반으로 변경

--- src/test/java/com/sz21c/common/TimeTest.java
+++ src/test/java/com/sz21c/common/TimeTest.java
@@ -2,6 +2,7 @@ package com.sz21c.common;
 
 import com.sz21c.common.config.SpringRootConfig;
 import com.sz21c.common.util.TimeUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -9,15 +10,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {SpringRootConfig.class})
 public class TimeTest {
 
-    Logger logger = LoggerFactory.getLogger(TimeTest.class);
-
     @Test
     public void testConvertMillisecondToDate() throws Exception {
         Long millisecond = TimeUtil.getCurrentMilliTime();
-        logger.info("current Time :: " + TimeUtil.getDateStringFromMilliTime(millisecond));
+        log.info("current Time :: " + TimeUtil.getDateStringFromMilliTime(millisecond));
     }
 }
